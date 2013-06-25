2301fece19a1a282ee22815ad16862ae73706e66
Bob Monteverde <bob@montev.com>
2013-06-25 18:40:57 -0400

    Changed firebase code because angularFire 0.0.0 has different API than 0.1.0 that I am using on the cdn

--- app/scripts/controllers/Chat.js
+++ app/scripts/controllers/Chat.js
@@ -6,7 +6,8 @@ angular.module('codedhereApp')
   .controller('ChatCtrl', ['$scope', '$timeout', 'angularFireCollection',
     function ($scope, $timeout, angularFireCollection) {
       var url = 'https://codedhere.firebaseio.com/chat';
-      $scope.messages = angularFireCollection(new Firebase(url).limit(50));
+      //$scope.messages = angularFireCollection(new Firebase(url).limit(50));
+      $scope.messages = angularFireCollection(url);
       $scope.username = 'Guest' + Math.floor(Math.random()*101);
       $scope.addMessage = function() {
         $scope.messages.add({from: $scope.username, content: $scope.message});
