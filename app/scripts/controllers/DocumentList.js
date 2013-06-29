853f8ebd7257c1ea2e3d947c4565c713576c1876
Bob Monteverde <bob@montev.com>
2013-06-29 12:36:54 -0400

    Updates to get Firebase running Documents and documnets auto saving and loading, collaborative funcitonality already working

--- app/scripts/controllers/DocumentList.js
+++ app/scripts/controllers/DocumentList.js
@@ -1,41 +1,12 @@
 'use strict';
 
 angular.module('codedhereApp')
-  .controller('DocumentListCtrl', ['$scope', function ($scope) {
-    $scope.documents = [
-      {
-        id: 1,
-        title: 'Document1',
-        description: 'A document description',
-        htmlCode: '<strong>test</strong>',
-        cssCode: 'strong { color: #f00 }',
-        jsCode: '$("strong").css("font-size": "20px");'
-      },
-      {
-        id: 2,
-        title: 'Document2',
-        description: 'Another document description',
-        htmlCode: '<b>test</b>',
-        cssCode: 'strong { color: #0f0 }',
-        jsCode: '$("b").css("font-size": "30px");'
-      },
-      {
-        id: 3,
-        title: 'Document3',
-        description: 'Yet another document description',
-        htmlCode: '<strong>test</strong>',
-        cssCode: 'strong { color: #f00 }',
-        jsCode: '$("strong").css("font-size": "20px");'
-      },
-      {
-        id: 4,
-        title: 'Document4',
-        description: 'One more document description',
-        htmlCode: '<strong>test</strong>',
-        cssCode: 'strong { color: #f00 }',
-        jsCode: '$("strong").css("font-size": "20px");'
-      }
-    ];
+  .controller('DocumentListCtrl', ['$scope', 'angularFireCollection',
+    function ($scope, angularFireCollection) {
+      var docsUrl = 'https://codedhere.firebaseio.com/documents';
+
+      $scope.documents = angularFireCollection(docsUrl);
+console.log($scope.documents);
 
     /*
     // Use when backend is up and running
--- app/scripts/controllers/Editor.js
+++ app/scripts/controllers/Editor.js
@@ -1,85 +1,35 @@
 'use strict';
 
 angular.module('codedhereApp')
-  .controller('EditorCtrl', ['$scope', '$routeParams', '$location', 'angularFireCollection',
-    function ($scope, $routeParams, $location, angularFireCollection) {
+  .controller('EditorCtrl', ['$scope', '$routeParams', '$location', '$timeout', 'angularFire', 'Firebase',
+    function ($scope, $routeParams, $location, $timeout, angularFire, Firebase) {
       var id = $routeParams.id,
-          docUrl = 'https://codedhere.firebaseio.com/documents',
-          //documentlist = angularFireCollection(docUrl);
-          documentlist = new Firebase(docUrl);
+          docUrl = 'https://codedhere.firebaseio.com/documents';
 
-      console.log('documents:', documentlist);
-      console.log('location path:', $location.path());
+      //console.log('documents:', documentlist);
+      //console.log('location path:', $location.path());
 
 
-      var blank = {
-        id: '',
-        title: '',
-        description: '',
-        htmlCode: '',
-        cssCode: '',
-        jsCode: ''
-      };
+      if (id === 'new') {
+         id = new Firebase(docUrl).push().name();
+         $location.path('/document/' + id);
+         return;
+      }
 
-      var documents = [
-        {
-          id: 1,
-          title: 'Document1',
-          description: 'A document description',
-          htmlCode: '<strong>test</strong>',
-          cssCode: 'strong { color: #f00 }',
-          jsCode: '$("strong").css("font-size", "20px");'
-        },
-        {
-          id: 2,
-          title: 'Document2',
-          description: 'Another document description',
-          htmlCode: '<b>test</b>',
-          cssCode: 'strong { color: #0f0 }',
-          jsCode: '$("b").css("font-size", "30px");'
-        },
-        {
-          id: 3,
-          title: 'Document3',
-          description: 'Yet another document description',
-          htmlCode: '<strong>test</strong>',
-          cssCode: 'strong { color: #f00 }',
-          jsCode: '$("strong").css("font-size", "20px");'
-        },
-        {
-          id: 4,
-          title: 'Document4',
-          description: 'One more document description',
-          htmlCode: '<strong>test</strong>',
-          cssCode: 'strong { color: #f00 }',
-          jsCode: '$("strong").css("font-size", "20px");'
-        }
-      ];
 
-      var doc = documents.filter(function(d) {
-        return +d.id === +id;
+      var promise = angularFire(docUrl + '/' + id, $scope, 'doc', {});
+
+
+      //$scope.$on('$viewContentLoaded', function() { $timeout(preview); });
+
+
+      promise.then(function() {
+        $timeout(preview);
       });
 
-      $scope.doc = doc.length ? doc[0] : blank;
-
-      $scope.$on('$viewContentLoaded', preview);
 
       $scope.preview = preview;
-      $scope.save = save;
 
-      function save() {
-
-        if (id === 'new') {
-          var newDoc = documentlist.push($scope.doc, function(err) {
-            if (!err) {
-              console.log('id:', newDoc.name());
-              //TODO: figure out why this does not work at all here!!
-              $location.path('/document/' + newDoc.name());
-            }
-          });
-
-        }
-      }
 
       function preview() {
 
