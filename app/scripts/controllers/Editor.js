0743e9f7e9eb31359b08f50db54e11e6cb9d9eec
Bob Monteverde <bob@montev.com>
2013-07-01 13:30:05 -0400

    Added ability to load any external css or js files with URL

--- app/scripts/controllers/Editor.js
+++ app/scripts/controllers/Editor.js
@@ -5,7 +5,8 @@ angular.module('codedhereApp')
     function ($scope, $routeParams, $location, $timeout, angularFire, Firebase) {
       var id = $routeParams.id,
           url = 'https://codedhere.firebaseio.com/',
-          docUrl = url + 'documents';
+          docUrl = url + 'documents',
+          blankPermissions = {public: 'r', users: [] };
 
       //console.log('documents:', documentlist);
       //console.log('location path:', $location.path());
@@ -23,13 +24,53 @@ angular.module('codedhereApp')
       //$scope.$on('$viewContentLoaded', function() { $timeout(preview); });
 
 
+
+
       promise.then(function() {
         $timeout(preview);
+
+        if (!$scope.doc.permissions)
+          $scope.doc.permissions = angular.copy(blankPermissions);
+
+        $scope.addUser = function () {
+          //TODO: figure out why users was getting deleted entirely (maybe firebase with empty arrays automatic?
+          if (!$scope.doc.permissions.users)
+            $scope.doc.permissions.users = [];
+
+          $scope.doc.permissions.users.push({'name': '', 'permissions': 'r'});
+        };
+
+        $scope.removeUser = function(user) {
+          var users = $scope.doc.permissions.users;
+          for (var i = 0, ii = users.length; i < ii; i++) {
+            if (user === users[i]) {
+              users.splice(i, 1);
+            }
+          }
+        };
+
+
+        $scope.addResource = function () {
+          //TODO: figure out why users was getting deleted entirely (maybe firebase with empty arrays automatic?
+          if (!$scope.doc.resources)
+            $scope.doc.resources = [];
+
+          $scope.doc.resources.push({'url': '', 'enabled': true});
+        };
+
+        $scope.removeResource = function(resource) {
+          var resources = $scope.doc.resources;
+          for (var i = 0, ii = resources.length; i < ii; i++) {
+            if (resource === resources[i]) {
+              resources.splice(i, 1);
+            }
+          }
+        };
+
       });
 
 
       $scope.preview = preview;
-
 
 
 
@@ -47,9 +88,18 @@ angular.module('codedhereApp')
         }
 
         frame.open();
+        if ($scope.doc.resources)
+          $scope.doc.resources.forEach(function(src) {
+            if (src.url.indexOf('.css') !== -1)
+              frame.write('<link rel="stylesheet" href="' + src.url + '">');
+          });
         frame.write('<style>' + $scope.doc.cssCode + '</style>');
         frame.write($scope.doc.htmlCode);
-        frame.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>');
+        if ($scope.doc.resources)
+          $scope.doc.resources.forEach(function(src) {
+            if (src.url.indexOf('.js') !== -1)
+              frame.write('<script src="' + src.url + '"></script>');
+          });
         frame.write('<script>' + $scope.doc.jsCode + '</script>');
         frame.close();
       }
